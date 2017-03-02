88c45c5e2af49c89e7c9dbc2beb9e81b8d5f2c54
Youssef Taghlabi <Youssef.Taghlabi@CapitalOne.com>
2017-03-02 09:19:33 -0500

    wip: fixed mocks

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -3,7 +3,6 @@ import { MockabilityResponses } from '@c1/mockability';
 import { requests } from './accounts';
 import { creditCardMocks } from './creditcard';
 
-export const mocks: MockabilityResponses = [
-  ...requests,
-  ...creditCardMocks
-];
+export function mocks():MockabilityResponses {
+  return [...requests, ...creditCardMocks];
+}
